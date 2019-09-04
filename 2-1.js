987a168720fcbd14b8fd0987178a38a5c63de680
freeman298 <54424225+freeman298@users.noreply.github.com>
2019-09-04 15:58:34 +0900

    Add files via upload

--- 2-1.js
+++ 2-1.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var router = require('./2-1-main')(app);
+
+app.set('views', _dirname+'/views');
+app.set('view engine','ejs');
 
 app.listen(3000,()=>{
     console.log("웹서버가 실행되었다. http://127.0.0.1:3000")
-})
+});
 
 app.get('/',(req,res)=>{
     res.send("안녕 서초")
-})
+});
